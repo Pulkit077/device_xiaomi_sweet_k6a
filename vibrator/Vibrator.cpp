ed12f62860eef5422fef27b6c4613e0948c705d0
Pulkit077 <pulkitagarwal2k1@gmail.com>
2023-12-08 21:22:35 +0530

    sweet_k6a: vibrator: add `awinic_haptic` support for sweet_k6a
    Signed-off-by: Pulkit077 <pulkitagarwal2k1@gmail.com>

--- vibrator/Vibrator.cpp
+++ vibrator/Vibrator.cpp
@@ -107,7 +107,7 @@ InputFFDevice::InputFFDevice()
         }
 
         if (strcmp(name, "qcom-hv-haptics") && strcmp(name, "qti-haptics") &&
-                strcmp(name, "aw8624_haptic")) {
+                strcmp(name, "aw8624_haptic") && strcmp(name, "awinic_haptic")) {
             ALOGD("not a qcom/qti haptics device\n");
             close(fd);
             continue;
